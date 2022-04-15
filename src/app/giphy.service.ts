55c672539bbb7a7b124fc1f64fa28dc0feb25aae
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-04-15 11:15:26 +0300

    patch:adding a bootstrap cdn link

--- src/app/giphy.service.ts
+++ src/app/giphy.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GiphyService {
-  baseurl: string='https://api.giphy.com/v1/gifs/trending?api_key=6oSF6eI4iFMe7UHY2o3BGTVnLdgUZRt7&limit=25&rating=g'
+  baseurl: string=`https://api.giphy.com/v1/gifs/trending?api_key=${environment.apiKey}&limit=25&rating=g`
 
   constructor(private http:HttpClient) { }
 }
