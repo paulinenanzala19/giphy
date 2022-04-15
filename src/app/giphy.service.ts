b5e2db6f8339b3011efe8ed3dad990adce0d4023
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-04-15 11:09:12 +0300

    adding the base url

--- src/app/giphy.service.ts
+++ src/app/giphy.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GiphyService {
+  baseurl: string='https://api.giphy.com/v1/gifs/trending?api_key=6oSF6eI4iFMe7UHY2o3BGTVnLdgUZRt7&limit=25&rating=g'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
